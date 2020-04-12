317ab7e26224043fe166830be7382bce7340462d
okapy <36099852+okadrive@users.noreply.github.com>
2020-04-13 03:48:50 +0900

    fix: template に適切な改行を入れた

--- sample/main.cpp
+++ sample/main.cpp
@@ -8,37 +8,111 @@ typedef long long ll;
 #define EACH(value, var) for (auto value : var)
 #define SORT(var) sort(var.begin(), var.end())
 #define REVERSE(var) reverse(var.begin(), var.end())
-#define RSORT(var) SORT(var); REVERSE(var)
-#define OPTIMIZE_STDIO ios::sync_with_stdio(false);cin.tie(0);cout.precision(10);cout << fixed
+#define RSORT(var) \
+  SORT(var);       \
+  REVERSE(var)
+#define OPTIMIZE_STDIO         \
+  ios::sync_with_stdio(false); \
+  cin.tie(0);                  \
+  cout.precision(10);          \
+  cout << fixed
 #define endl '\n'
 
 const int INF = 1e9;
 const ll MOD = 1e9 + 7;
 const ll LINF = 1e18;
 struct mint {
-    ll x;
-    mint() : x(0) {}
-    mint(ll x) : x((x % MOD + MOD) % MOD) {}
-    mint& operator+=(const mint& r) { if ((x += r.x) >= MOD) { x -= MOD; } return *this; }
-    mint& operator-=(const mint& r) { if ((x -= r.x) < 0) { x += MOD; } return *this; }
-    mint& operator*=(const mint& r) { if ((x *= r.x) > MOD) { x %= MOD; } return *this; }
-    mint& operator++() { if ((++x) >= MOD) { x -= MOD; } return *this; }
-    mint operator++(int) { mint ret = x; if ((++x) >= MOD) { x-= MOD; } return ret; }
-    mint& operator--() { if ((--x) < 0) { x += MOD; } return *this; }
-    mint operator--(int) { mint ret = x; if ((--x) < 0) { x += MOD; } return ret; }
-    mint operator+(const mint& r) { mint ret; return ret = x + r.x; }
-    mint operator-(const mint& r) { mint ret; return ret = x - r.x; }
-    mint operator*(const mint& r) { mint ret; return ret = x * r.x; }
-    mint operator-() { return mint() - *this; }
-    bool operator<(const mint& a) { return x < a.x; }
-    bool operator>(const mint& a) { return x > a.x; }
-    bool operator<=(const mint& a) { return x <= a.x; }
-    bool operator>=(const mint& a) { return x >= a.x; }
-    bool operator==(const mint& a) { return x == a.x; }
-    bool operator!=(const mint& a) { return x != a.x; }
+  ll x;
+  mint() : x(0) {
+  }
+  mint(ll x) : x((x % MOD + MOD) % MOD) {
+  }
+  mint& operator+=(const mint& r) {
+    if ((x += r.x) >= MOD) {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint& operator-=(const mint& r) {
+    if ((x -= r.x) < 0) {
+      x += MOD;
+    }
+    return *this;
+  }
+  mint& operator*=(const mint& r) {
+    if ((x *= r.x) > MOD) {
+      x %= MOD;
+    }
+    return *this;
+  }
+  mint& operator++() {
+    if ((++x) >= MOD) {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint operator++(int) {
+    mint ret = x;
+    if ((++x) >= MOD) {
+      x -= MOD;
+    }
+    return ret;
+  }
+  mint& operator--() {
+    if ((--x) < 0) {
+      x += MOD;
+    }
+    return *this;
+  }
+  mint operator--(int) {
+    mint ret = x;
+    if ((--x) < 0) {
+      x += MOD;
+    }
+    return ret;
+  }
+  mint operator+(const mint& r) {
+    mint ret;
+    return ret = x + r.x;
+  }
+  mint operator-(const mint& r) {
+    mint ret;
+    return ret = x - r.x;
+  }
+  mint operator*(const mint& r) {
+    mint ret;
+    return ret = x * r.x;
+  }
+  mint operator-() {
+    return mint() - *this;
+  }
+  bool operator<(const mint& a) {
+    return x < a.x;
+  }
+  bool operator>(const mint& a) {
+    return x > a.x;
+  }
+  bool operator<=(const mint& a) {
+    return x <= a.x;
+  }
+  bool operator>=(const mint& a) {
+    return x >= a.x;
+  }
+  bool operator==(const mint& a) {
+    return x == a.x;
+  }
+  bool operator!=(const mint& a) {
+    return x != a.x;
+  }
 };
-ostream& operator<<(ostream& os, const mint& r) { os << r.x; return os; }
-ostream& operator>>(ostream& os, const mint& r) { os >> r.x; return os; }
+ostream& operator<<(ostream& os, const mint& r) {
+  os << r.x;
+  return os;
+}
+ostream& operator>>(ostream& os, const mint& r) {
+  os >> r.x;
+  return os;
+}
 
 void solve(istream& cin, ostream& cout) {
 }
