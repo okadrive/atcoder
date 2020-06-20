61d7595d103c57a15b56773c2cd205362ab9a920
okapy <36099852+okadrive@users.noreply.github.com>
2020-06-20 14:31:21 +0900

    add: 素因数分解

---
+++ lib/sieve.cpp
@@ -0,0 +1,16 @@
+using P = pair<int, int>;
+
+// 素因数分解の結果をpair型で返す
+vector<P> decompositPrime(int n) {
+  vector<P> res;
+  int s = sqrt(n);
+  FOR(i, 2, s) {
+    int p = 0;
+    while (n % i == 0) {
+      p++;
+      n /= i;
+    }
+    if (p != 0) res.emplace_back(i, p);
+  }
+  return res;
+}
